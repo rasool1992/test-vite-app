268ee9503df781bcf7d32bed7b0b35d410cc3f51
rasool1992 <somanxx@yahoo.com>
2024-08-05 15:29:52 +0300

    ..

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/test-vite-app/", // your repository name
+  base: "/test-vite-app/", // Make sure this matches your repo name
 });
